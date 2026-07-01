ea47afd237c08e04e571db9704e7806dd4f4f85c
manivannancsmca <manivannancsmca@gmail.com>
2026-07-01 15:39:40 +0530

    Serializable added for order response dto class

--- order-service/src/main/java/com/order_service/dto/OrderResponse.java
+++ order-service/src/main/java/com/order_service/dto/OrderResponse.java
@@ -1,13 +1,20 @@
 package com.order_service.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Data
 @Builder
-public class OrderResponse {
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderResponse implements Serializable {
+    private static final long serialVersionUID = 1L;
     private Long id;
     private Long productId;
     private Integer quantity;
